f9e497409479f22e880b1ee28997a4a460e5e3a1
SkyShadowHero <a1233456333@163.com>
2023-08-20 14:31:48 +0800

    Site updated: 2023-08-20 14:31:26

--- js/sakana-init.js
+++ js/sakana-init.js
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded", () => {
   };
   document.body.appendChild(script);
 });
-
+// 公告
 var typed = new Typed('#type-container', {
   strings: ['我是公告，但这个博客似乎^1000没有公告:('],
   showCursor: true, // 是否显示光标
@@ -26,10 +26,20 @@ var typed = new Typed('#type-container', {
   fadeOut: true,
   fadeOutClass: 'typed-cursor',
 });
+// Hello World
 var typed = new Typed('#hello-world', {
   strings: ['Hello^3000 World!'],
   showCursor: true, // 是否显示光标
   cursorChar: '<i class="iconfont icon-sword"></i>', // 光标的样式
   typeSpeed: 250,
+  startDelay: 3000,
+});
+// 帖子里的
+var typed = new Typed('#test1', {
+  strings: [
+    '能力越大，责任越大',
+    '出自《蜘蛛侠》',
+  ],
+  showCursor: true, // 是否显示光标
   startDelay: 3000,
 });
